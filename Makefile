f3b00e79e4a73f95ea5d332d04dfcd169be7cb90
David Sennerlöv <ndavidn@gmail.com>
2019-02-04 22:24:26 +0100

    Add examples for how event_sourced is used

---
+++ Makefile
@@ -0,0 +1,4 @@
+
+.PHONY: examples
+examples:
+	@bundle exec ruby ./examples/01_simple_repo/01_simple_repo.rb
---
+++ examples/01_simple_repo/commands.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'event_sourced/command'
+
+class CreateInventoryItem < EventSourced::Command
+  field :description
+  field :count
+
+  builder do |data|
+    self.description = data[:description]
+    self.count       = data[:count]
+  end
+end
+
+class UpdateInventoryItem < EventSourced::Command
+  field :description
+
+  builder do |data|
+    self.description = data[:description]
+  end
+end
+
+class RestockInventoryItem < EventSourced::Command
+  field :count
+
+  builder do |data|
+    self.count = data[:count]
+  end
+end
+
+class WithdrawInventoryItem < EventSourced::Command
+  field :count
+
+  builder do |data|
+    self.count = data[:count]
+  end
+end
