5934a8180dc8ba6ca0a64476ee7f63db27363ca3
David Sennerlöv <ndavidn@gmail.com>
2019-02-04 22:31:14 +0100

    Add test cases

--- test/event_sourced_test.rb
+++ test/event_sourced_test.rb
@@ -5,7 +5,7 @@ class EventSourcedTest < Minitest::Test
     refute_nil ::EventSourced::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
-  end
+  # def test_it_does_something_useful
+  #   assert false
+  # end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,10 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "event_sourced"
 
 require "minitest/autorun"
+require 'minitest/reporters'
+
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(color: true, slow_count:  5)]
+
+class MiniTest::Test
+  extend MiniTest::Spec::DSL
+end
