f175cdc439ef8b5818c594a2cb434b4443b26d30
David Sennerlöv <ndavidn@gmail.com>
2019-04-19 15:51:22 +0200

    Link more files

--- lib/event_sourced.rb
+++ lib/event_sourced.rb
@@ -1,3 +1,5 @@
+#activesupport/lib/active_support/core_ext/hash/keys.rb,
+require 'active_support/core_ext/hash'
 require "event_sourced/version"
 require "event_sourced/event"
 require "event_sourced/command"
@@ -5,7 +7,10 @@ require "event_sourced/command_repository"
 require "event_sourced/event_repository"
 require "event_sourced/command_handler"
 require "event_sourced/aggregate_root"
+require "event_sourced/projection"
 require "event_sourced/repositories/memory_repository"
+require "event_sourced/repositories/mongo_repository"
+#require "event_sourced/repositories/dynamo_repository"
 
 module EventSourced
   # Your code goes here...
