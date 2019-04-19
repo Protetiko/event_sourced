e8ca7fba466fd249eba787d3df383a2162b986c1
David Sennerlöv <ndavidn@gmail.com>
2019-04-19 11:57:45 +0200

    Update MongoRepository with dynamic DB and Collection names

--- lib/event_sourced/repositories/mongo_repository.rb
+++ lib/event_sourced/repositories/mongo_repository.rb
@@ -2,50 +2,66 @@
 
 require 'mongo'
 require 'json'
-require 'event_sourced/repository'
-require_relative 'event.rb'
 
-class MongoRepository
-  module Support
-    def create_indexes
-      result = collection.indexes.create_one({aggregate_id: 1})
-      result = collection.indexes.create_one({aggregate_id: 1, timestamp: 1})
+module EventSourced
+  class MongoRepository
+    include EventSourced::Repository
+
+    module Support
+      def create_indexes
+        result = collection.indexes.create_one({aggregate_id: 1})
+        result = collection.indexes.create_one({aggregate_id: 1, timestamp: 1})
+      end
     end
-  end
 
-  include EventSourced::Repository
-
-  def append(event)
-    raise InvalidEvent.new unless event.is_a? Event
-    result = collection.insert_one(event.to_h)
-  end
-
-  def append_many(events)
-    raise InvalidEvent.new unless event.is_a? Array
-    result = collection.insert_many(events)
-  end
-
-  def events(aggregate_id)
-    return collection.find(aggregate_id) # order by timestamp
-  end
-
-  def aggregate(aggregate_id)
-    result = events(aggregate_id)
-    result.each do |document|
+    def initialize(config)
+      @database = config[:database] || 'event-store'
+      @collection_name = config[:collection] || 'events'
     end
-  end
 
-  private
+    def append(event)
+      #raise InvalidEvent.new unless event.is_a? EventSourced::Message
+      result = collection.insert_one(event.to_h)
+    end
 
-  def client
-    @client ||= Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'order-event-store')
-  end
+    def append_many(events)
+      raise InvalidEventCollection.new unless event.is_a? Array
+      result = collection.insert_many(events)
+    end
 
-  def db
-    @db ||= client.database
-  end
+    def events(aggregate_id)
+      return collection.find(aggregate_id) # order by timestamp
+    end
 
-  def collection
-    @collection ||= client['events']
+    def aggregate(aggregate_id)
+      result = events(aggregate_id)
+      result.each do |document|
+      end
+    end
+
+    def stream(aggregate_id)
+      collection.find(aggregate_id: aggregate_id)
+    end
+
+    def dump
+      collection.find.each_with_index do |record, i|
+        puts i
+        puts record.to_h
+      end
+    end
+
+    private
+
+    def client
+      @client ||= Mongo::Client.new([ '127.0.0.1:27017' ], :database => @database)
+    end
+
+    def db
+      @db ||= client.database
+    end
+
+    def collection
+      @collection ||= client[@collection_name]
+    end
   end
 end
