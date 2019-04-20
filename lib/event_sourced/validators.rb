6a440b8e4e36894c5b5b0de7b817071b5a9ec06c
David Sennerlöv <ndavidn@gmail.com>
2019-04-20 22:05:08 +0200

    Update and link Validators

--- lib/event_sourced/validators.rb
+++ lib/event_sourced/validators.rb
@@ -1,4 +1,22 @@
 # frozen_string_literal: true
 
+require 'dry-validation'
+
+module EventSourced
+  module Validators
+    class BaseSchema < Dry::Validation::Schema
+      configure do |_|
+        def included_in_case_ignored?(list, input)
+          list.any?{ |s| s.casecmp(input) == 0 }
+        end
+
+        def id?(value)
+          value.class == String
+        end
+      end
+    end
+  end
+end
+
 require 'event_sourced/validators/command_validator'
 require 'event_sourced/validators/event_validator'
--- lib/event_sourced/validators/event_validator.rb
+++ lib/event_sourced/validators/event_validator.rb
@@ -1,16 +1,14 @@
 # frozen_string_literal: true
 
-require 'dry-validation'
-
 module EventSourced
   module Validators
-    EventValidator = Dry::Validation.Schema do
-      required(:command_id).filled?(:str?)
-      required(:aggregate_id).filled?(:str?)
-      required(:command_type).filled?(:str?)
-      required(:data).filled?(:str?)
-      required(:meta_data).filled?(:str?)
-      required(:created_at).filled?(:str?)
+    EventValidator = Dry::Validation.Params(Validators::BaseSchema) do
+      optional(:command_id).filled(:str?)
+      required(:aggregate_id).filled(:str?)
+      optional(:correlation_id).filled(:str?)
+      required(:data).filled(:hash?)
+      required(:meta_data).filled(:hash?)
+      required(:timestamp).filled(:str?)
     end
   end
 end
