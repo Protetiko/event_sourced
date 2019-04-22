a47d2873195d18d508acb6124fc22ced86cdf20e
David Sennerlöv <ndavidn@gmail.com>
2019-04-22 11:25:37 +0200

    Update record attributes

--- lib/event_sourced/models/command_record.rb
+++ lib/event_sourced/models/command_record.rb
@@ -6,19 +6,19 @@ module EventSourced
       :aggregate_id,
       :aggregate_type,
       :command_id,
-      :command_type,
+      :type,
       :correlation_id,
       :causation_id,
       :data,
       :meta_data,
       :created_at,
-      :command_version,
+      :version,
     )
 
     CommandValidator = Dry::Validation.Schema do
       required(:command_id).filled?(:str?)
       required(:aggregate_id).filled?(:str?)
-      required(:command_type).filled?(:str?)
+      required(:type).filled?(:str?)
       required(:data).filled?(:hash?)
       required(:meta_data).filled?(:hash?)
       required(:created_at).filled?(:str?)
--- lib/event_sourced/models/event_record.rb
+++ lib/event_sourced/models/event_record.rb
@@ -6,21 +6,21 @@ module EventSourced
       :aggregate_id,
       :aggregate_type,
       :sequence,
-      :event_type,
+      :type,
       :command_id,
       :correlation_id,
       :causation_id,
       :timestamp,
       :data,
       :meta_data,
-      :event_version,
+      :version,
     )
 
     EventValidator = Dry::Validation.Schema do
       required(:aggregate_id).filled?(:str?)
       required(:aggregate_type).filled?(:str?)
       required(:sequence).filled?(:int?)
-      required(:event_type).filled?(:str?)
+      required(:type).filled?(:str?)
       required(:command_id).filled?(:str?)
       required(:correlation_id).filled?(:str?)
       required(:causation_id).filled?(:str?)
