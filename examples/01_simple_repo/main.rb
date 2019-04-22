fe9ab16bd627cadc421d3a5c1b46be4a93502268
David Sennerlöv <ndavidn@gmail.com>
2019-04-22 11:23:13 +0200

    Update examples

--- examples/01_simple_repo/main.rb
+++ examples/01_simple_repo/main.rb
@@ -6,20 +6,33 @@ require_relative '../_common/commands.rb'
 require_relative '../_common/events.rb'
 require_relative '../_common/inventory_command_handler.rb'
 require_relative '../_common/inventory_item.rb'
-require_relative '../_common/command_messages.rb'
+require_relative '../_common/messages.rb'
 
-event_repository   = EventSourced::Repository.new(store: EventSourced::EventStores::MemoryEventStore.new)
-command_repository = EventSourced::Repository.new(store: EventSourced::EventStores::MemoryEventStore.new)
+event_repository   = EventSourced::Repository.new(
+  aggregate: InventoryItem,
+  factory: EventSourced::Event::Factory,
+  event_store: EventSourced::EventStores::MemoryEventStore.new
+)
+command_repository = EventSourced::Repository.new(
+  aggregate: InventoryItem,
+  factory: EventSourced::Event::Factory,
+  event_store: EventSourced::EventStores::MemoryEventStore.new
+)
 command_handler    = InventoryCommandHandler.new(event_repository, command_repository)
 
 command_handler.handle(CreateInventoryItem.new(CREATE_ITEM_COMMAND_MESSAGE))
 10.times { command_handler.handle(WithdrawInventoryItem.new(WITHDRAW_ITEM_COMMAND_MESSAGE)) }
 command_handler.handle(RestockInventoryItem.new(RESTOCK_ITEM_COMMAND_MESSAGE))
 command_handler.handle(UpdateInventoryItem.new(UPDATE_ITEM_COMMAND_MESSAGE))
-command_handler.handle(WithdrawInventoryItem.new(WITHDRAW_ITEM_COMMAND_MESSAGE.merge({data:{count:230}})))
+command_handler.handle(WithdrawInventoryItem.new(WITHDRAW_ITEM_COMMAND_MESSAGE.merge({ data: { count: 230 } })))
 
-command_repository.dump
-event_repository.dump
+# command_repository.dump
+# event_repository.dump
 
 item = InventoryItem.new(event_repository)
 ap item.load(AGGREGATE_ID).to_h
+ap event_repository.aggregate(AGGREGATE_ID).to_h
+
+
+ap EventSourced::Command::Factory.for('CreateInventoryItem')
+ap EventSourced::Event::Factory.build('InventoryItemCreated', INVENTORY_ITEM_CREATED).to_h
--- examples/02_simple_mongo_repo/main.rb
+++ examples/02_simple_mongo_repo/main.rb
@@ -6,13 +6,20 @@ require_relative '../_common/commands.rb'
 require_relative '../_common/events.rb'
 require_relative '../_common/inventory_command_handler.rb'
 require_relative '../_common/inventory_item.rb'
-require_relative '../_common/command_messages.rb'
+require_relative '../_common/messages.rb'
 
 Mongo::Logger.logger.level = Logger::WARN
 
-
-event_repository   = EventSourced::Repository.new(store: EventSourced::EventStores::MongoEventStore.new(collection: 'item-events'))
-command_repository = EventSourced::Repository.new(store: EventSourced::EventStores::MongoEventStore.new(collection: 'item-commands'))
+event_repository   = EventSourced::Repository.new(
+  aggregate: InventoryItem,
+  factory: EventSourced::Event::Factory,
+  event_store: EventSourced::EventStores::MongoEventStore.new(collection: 'item-events')
+)
+command_repository = EventSourced::Repository.new(
+  aggregate: InventoryItem,
+  factory: EventSourced::Command::Factory,
+  event_store: EventSourced::EventStores::MongoEventStore.new(collection: 'item-commands')
+)
 command_handler    = InventoryCommandHandler.new(event_repository, command_repository)
 
 event_repository.drop_all!
@@ -22,10 +29,14 @@ command_handler.handle(CreateInventoryItem.new(CREATE_ITEM_COMMAND_MESSAGE))
 10.times { command_handler.handle(WithdrawInventoryItem.new(WITHDRAW_ITEM_COMMAND_MESSAGE)) }
 command_handler.handle(RestockInventoryItem.new(RESTOCK_ITEM_COMMAND_MESSAGE))
 command_handler.handle(UpdateInventoryItem.new(UPDATE_ITEM_COMMAND_MESSAGE))
-command_handler.handle(WithdrawInventoryItem.new(WITHDRAW_ITEM_COMMAND_MESSAGE.merge({data:{count:230}})))
+command_handler.handle(WithdrawInventoryItem.new(WITHDRAW_ITEM_COMMAND_MESSAGE.merge({ data: { count: 230 } })))
 
-command_repository.dump
-event_repository.dump
+# command_repository.dump
+# event_repository.dump
 
 item = InventoryItem.new(event_repository)
 ap item.load(AGGREGATE_ID).to_h
+ap event_repository.aggregate(AGGREGATE_ID).to_h
+
+ap EventSourced::Command::Factory.for('CreateInventoryItem')
+ap EventSourced::Event::Factory.build('InventoryItemCreated', INVENTORY_ITEM_CREATED).to_h
