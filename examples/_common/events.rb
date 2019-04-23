283d6a67c870b7f58037e599e20eecf27342898f
David Sennerlöv <ndavidn@gmail.com>
2019-04-23 11:11:24 +0200

    Small random updates

--- examples/_common/events.rb
+++ examples/_common/events.rb
@@ -8,6 +8,10 @@ class InventoryItemCreated < EventSourced::Event
   builder do |data|
     self.description = data[:description]
   end
+
+  def to_s
+    "#{self.class.name} - Set description to `#{self.description}`"
+  end
 end
 
 class InventoryItemUpdated < EventSourced::Event
@@ -15,6 +19,10 @@ class InventoryItemUpdated < EventSourced::Event
 
   builder do |data|
     self.description = data[:description]
+  end
+
+  def to_s
+    "#{self.class.name} - Set description to `#{self.description}`"
   end
 end
 
@@ -24,6 +32,10 @@ class InventoryItemRestocked < EventSourced::Event
   builder do |data|
     self.count = data[:count]
   end
+
+  def to_s
+    "#{self.class.name} - Increase stock to `#{self.count}`"
+  end
 end
 
 class InventoryItemWithdrawn < EventSourced::Event
@@ -31,5 +43,9 @@ class InventoryItemWithdrawn < EventSourced::Event
 
   builder do |data|
     self.count = data[:count]
+  end
+
+  def to_s
+    "#{self.class.name} - Decrease stock to `#{self.count}`"
   end
 end
