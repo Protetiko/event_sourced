a2bbfec5be4257707fd930aedf76a647d5ac53b1
David Sennerlöv <ndavidn@gmail.com>
2019-04-23 11:10:58 +0200

    Update test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,9 +2,13 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'event_sourced'
 
 require 'minitest/autorun'
+require 'minitest/hell' # Enables parallel (multithreaded) execution for all tests.
 require 'minitest/reporters'
 
 Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(color: true, slow_count:  5)]
+
+require 'examples/inventory_item'
+#Dir[File.join(__dir__, 'examples', '*')].each { |file| require file }
 
 class MiniTest::Test
   extend MiniTest::Spec::DSL
