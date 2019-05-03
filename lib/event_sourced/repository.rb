e4ed5858d1b0bd44f8a19e9cc7f978b69693b638
David Sennerlöv <ndavidn@gmail.com>
2019-05-03 13:34:30 +0200

    Update Repository with new EventStores

--- lib/event_sourced/repository.rb
+++ lib/event_sourced/repository.rb
@@ -16,12 +16,43 @@ module EventSourced
       @event_store     = event_store
     end
 
-    def append(record)
-      event_store.append(record)
+    def create_aggregate(attributes)
+      attributes[:created_at] = current_timestamp
+      attributes = Validators::AggregateRecord.validate!(attributes)
+
+      event_store.create_aggregate(attributes)
+
+      return AggregateRecord.new(attributes)
     end
 
-    def append_many(records)
-      event_store.append_many(records)
+    def read_aggregate(aggregate_id)
+      return nil unless aggregate_id
+
+      aggregate = event_store.read_aggregate(aggregate_id)
+
+      return AggregateRecord.new(aggregate)
+    end
+
+    def save_snapshot(aggregate_root)
+      raise InvalidAggregateRoot unless aggregate_root.kind_of?(AggregateRoot)
+
+      snapshot = Validators::SnapshotRecord.validate!(aggregate_root.to_h)
+      snapshot[:created_at] = current_timestamp
+
+      result = event_store.save_snapshot(snapshot)
+      event_store.update_aggregate(aggregate_id, { last_snapshot_id: result[:id] })
+    end
+
+    def append_command(command)
+      event_store.append_command(command.to_h)
+    end
+
+    def append_event(event)
+      event_store.append_event(event.to_h)
+    end
+
+    def append_events(events)
+      event_store.append_events(events)
     end
 
     # This is only available for EventRepository
@@ -30,12 +61,12 @@ module EventSourced
       aggregate.load_from_events(stream(aggregate_id))
     end
 
-    def raw_stream(aggregate_id)
-      event_store.read_stream(aggregate_id)
+    def raw_event_stream(aggregate_id)
+      event_store.event_stream(aggregate_id)
     end
 
-    def stream(aggregate_id)
-      raw_stream(aggregate_id).map do |record|
+    def event_stream(aggregate_id)
+      raw_event_stream(aggregate_id).map do |record|
         @factory.build!(record[:type], record)
       end
     end
@@ -58,6 +89,10 @@ module EventSourced
     end
 
     private
+
+    def current_timestamp
+      Time.now.utc.round(3)
+    end
 
     def event_store
       @event_store || raise(EventStoreNotConfigured)
