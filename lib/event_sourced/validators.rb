538270b189f7497b9ac4690de445b662ac1e68fc
David Sennerlöv <ndavidn@gmail.com>
2019-05-06 23:59:17 +0200

    Refactor Validators::Base

--- lib/event_sourced/validators.rb
+++ lib/event_sourced/validators.rb
@@ -4,29 +4,35 @@ require 'dry-validation'
 
 module EventSourced
   module Validators
+    ValidationFailed = Class.new(StandardError)
+    ValidatorNotSet = Class.new(StandardError)
+    InvalidValidator = Class.new(StandardError)
+
     class Base
-      ValidationFailed = Class.new(StandardError)
-      ValidatorNotSet = Class.new(StandardError)
-      InvalidValidator = Class.new(StandardError)
+      module ClassMethods
+        def set_validator(v)
+          raise(InvalidValidator, 'Validator must respond to :call method') unless v.respond_to?(:call)
+          @validator = v
+        end
 
-      def self.set_validator(v)
-        raise(InvalidValidator, 'Validator must respond to :call method') unless v.respond_to?(:call)
-        @validator = v
+        def validator
+          @validator || raise(ValidatorNotSet, "Internal validator not set for #{self.name}")
+        end
+
+        def valid?(message)
+          validator.call(message).success?
+        end
+
+        def validate!(message)
+          result = validator.call(message)
+          raise(ValidationFailed, "#{self.name} failed validation: #{result.errors}") if result.failure?
+
+          return result.output # return sanitized result
+        end
       end
 
-      def self.validator
-        @validator || raise(ValidatorNotSet, "Internal validator not set for #{self.name}")
-      end
-
-      def self.valid?(message)
-        validator.call(message).success?
-      end
-
-      def self.validate!(message)
-        result = validator.call(message)
-        raise(ValidationFailed, "#{self.name} failed validation: #{result.errors}") if result.failure?
-
-        return result.output # return sanitized result
+      def self.inherited(base)
+        base.extend(ClassMethods)
       end
     end
 
@@ -46,5 +52,5 @@ end
 
 require 'event_sourced/validators/command_message'
 require 'event_sourced/validators/event_message'
-require 'event_sourced/validators/aggregate_record'
 require 'event_sourced/validators/snapshot_record'
+require 'event_sourced/validators/aggregate_record'
