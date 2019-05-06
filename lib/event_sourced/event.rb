983552b1dfe78136208e3c8778f6fa2217a9f3aa
David Sennerlöv <ndavidn@gmail.com>
2019-05-06 23:56:29 +0200

    Clean up Event

--- lib/event_sourced/event.rb
+++ lib/event_sourced/event.rb
@@ -13,50 +13,49 @@ module EventSourced
 
     include EventSourced::Message
 
-    EventValidationFailed = Class.new(StandardError)
-
+    attr_accessor :id
     attr_accessor :command_id
     attr_accessor :aggregate_id
+    attr_accessor :aggregate_type
     attr_accessor :type
     attr_accessor :data
     attr_accessor :meta_data
     attr_accessor :timestamp
-    attr_accessor :sequence
-    attr_accessor :version
+    attr_accessor :event_sequence_number
     attr_accessor :correlation_id
     attr_accessor :causation_id
 
     def initialize(event_message = {})
       event_message = Validators::EventMessage.validate!(event_message)
 
-      self.aggregate_id    = event_message[:aggregate_id]
-      self.command_id      = event_message[:command_id]
-      self.correlation_id  = event_message[:correlation_id] || self.command_id
-      self.causation_id    = self.command_id
-      self.type            = self.class.name
+      @type                  = self.class.name
+      @aggregate_id          = event_message[:aggregate_id]
+      @aggregate_type        = event_message[:aggregate_type]
+      @command_id            = event_message[:command_id]
+      @correlation_id        = event_message[:correlation_id] || @command_id
+      @causation_id          = @command_id
+      @event_sequence_number = event_message[:event_sequence_number]
 
       timestamp = event_message[:timestamp] || DateTime.now.utc.round(3)
       timestamp = DateTime.parse(timestamp) if timestamp.is_a?(String)
-      self.timestamp       = timestamp
+      @timestamp = timestamp
 
-      self.sequence        = event_message[:sequence]
-      self.version         = event_message[:version] || 1
-      self.meta_data       = event_message[:meta_data]
-
-      self.instance_exec(event_message[:data], &self.class._builder) if self.class._builder
-      self.data            = attributes
+      # Set the internal `attributes` variable
+      self.instance_exec(event_message[:data].symbolize_keys, &self.class._builder) if self.class._builder
+      @data      = attributes
+      @meta_data = event_message[:meta_data]
     end
 
     def to_h
       h = {
-        aggregate_id:    aggregate_id,
-        command_id:      command_id,
-        type:            type,
-        timestamp:       timestamp,
-        correlation_id:  correlation_id,
-        causation_id:    causation_id,
-        sequence:        sequence,
-        version:         version,
+        type:                  type,
+        aggregate_id:          aggregate_id,
+        aggregate_type:        aggregate_type,
+        command_id:            command_id,
+        correlation_id:        correlation_id,
+        causation_id:          causation_id,
+        timestamp:             timestamp,
+        event_sequence_number: event_sequence_number,
       }
 
       h[:data]      = attributes if attributes.present?
