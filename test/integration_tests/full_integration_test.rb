94000d4924d8e80a0ff6c42131524984882fb4b8
David Sennerlöv <ndavidn@gmail.com>
2019-08-17 23:05:24 +0200

    Update FullIntegrationTest

--- test/integration_tests/full_integration_test.rb
+++ test/integration_tests/full_integration_test.rb
@@ -3,13 +3,13 @@
 require 'test_helper'
 
 event_store ||= EventSourced::EventStores::MongoEventStore.new(aggregate_name: 'item')
-InventoryItem.event_store = event_store
+Item.event_store = event_store
 
 class FullIntegrationTest < MiniTest::Test
-  let(:event_store) { InventoryItem.event_store }
+  let(:event_store) { Item.event_store }
 
   let(:command_handler) {
-    InventoryCommandHandler.new(InventoryItem)
+    InventoryCommandHandler.new(Item)
   }
 
   let(:aggregate_id) {
@@ -21,40 +21,40 @@ class FullIntegrationTest < MiniTest::Test
   end
 
   def test_it_handles_single_commands
-    command = CreateInventoryItem.new(CREATE_ITEM_COMMAND_MESSAGE.merge(aggregate_id: aggregate_id))
+    command = CreateItem.new(CREATE_ITEM_COMMAND_MESSAGE.merge(aggregate_id: aggregate_id))
 
     command_handler.handle(command)
 
     command_stream = event_store.command_stream(aggregate_id)
     assert_equal 1, command_stream.size
 
-    assert_equal command.to_h.sort, CreateInventoryItem.new(command_stream.last).to_h.sort
-    assert_equal 'CreateInventoryItem', command_stream.last[:type]
+    assert_equal command.to_h.sort, CreateItem.new(command_stream.last).to_h.sort
+    assert_equal 'CreateItem', command_stream.last[:type]
 
     event_stream = event_store.event_stream(command.aggregate_id)
     assert_equal 2, event_stream.size
-    assert_equal 'InventoryItemCreated', event_stream.first[:type]
-    assert_equal 'InventoryItemRestocked', event_stream.last[:type]
+    assert_equal 'ItemCreated', event_stream.first[:type]
+    assert_equal 'InventoryRestocked', event_stream.last[:type]
   end
 
   def test_it_handles_multiple_commands
     command_handler.handle_commands(
       [
-        CreateInventoryItem.new(CREATE_ITEM_COMMAND_MESSAGE.merge(aggregate_id: aggregate_id)),
-        UpdateInventoryItem.new(UPDATE_ITEM_COMMAND_MESSAGE.merge(aggregate_id: aggregate_id)),
-        RestockInventoryItem.new(RESTOCK_ITEM_COMMAND_MESSAGE.merge(aggregate_id: aggregate_id))
+        CreateItem.new(CREATE_ITEM_COMMAND_MESSAGE.merge(aggregate_id: aggregate_id)),
+        SetDescription.new(SET_ITEM_DESCRIPTION_COMMAND_MESSAGE.merge(aggregate_id: aggregate_id)),
+        RestockInventory.new(RESTOCK_ITEM_COMMAND_MESSAGE.merge(aggregate_id: aggregate_id))
       ]
     )
 
     command_stream = event_store.command_stream(aggregate_id)
 
     assert_equal 3, command_stream.size
-    assert_equal 'CreateInventoryItem', command_stream.first[:type]
-    assert_equal 'RestockInventoryItem', command_stream.last[:type]
+    assert_equal 'CreateItem', command_stream.first[:type]
+    assert_equal 'RestockInventory', command_stream.last[:type]
 
     event_stream = event_store.event_stream(aggregate_id)
     assert_equal 4, event_stream.size
-    assert_equal 'InventoryItemCreated', event_stream.first[:type]
-    assert_equal 'InventoryItemRestocked', event_stream.last[:type]
+    assert_equal 'ItemCreated', event_stream.first[:type]
+    assert_equal 'InventoryRestocked', event_stream.last[:type]
   end
 end
