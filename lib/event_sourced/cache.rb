6af113d833c84d6a2659be56f9ca26a483324dcf
dsenner <ndavidn@gmail.com>
2019-08-22 17:53:33 +0200

    Add Cache class

---
+++ lib/event_sourced/cache.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module EventSourced
+  class Cache
+    def initialize(backend)
+      @cache_backend = backend
+    end
+
+    def get(id, &block)
+      return @cache_backend.get(id) if @cache_backend.key?(id)
+      return nil unless block_given?
+
+      result = yield(block)
+      put(id, result) if result
+      return result
+    end
+
+    def put(id, data)
+      @cache_backend.put(id, data)
+    end
+  end
+end
